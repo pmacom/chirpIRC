e054fe47c7fb1002a15f3932d8f0f9d3ac3cffbd
Patrick Macom <patrickmacom@Patricks-MacBook-Pro.local>
2020-11-16 13:26:00 -0500

    feat: allow for multiple channels in docker-compose envs

--- chirp/src/index.js
+++ chirp/src/index.js
@@ -6,7 +6,7 @@ var client = require('coffea')({
   ssl: false, // set to true if you want to use ssl
   ssl_allow_invalid: false, // set to true if the server has a custom ssl certificate
   prefix: '!', // used to parse commands and emit on('command') events, default: !
-  channels: [process.env.CHIRPIRC_CHANNEL], // autojoin channels, default: []
+  channels: [process.env.CHIRPIRC_CHANNEL.split(',')], // autojoin channels, default: []
   nick: process.env.CHIRPIRC_NICK, // default value: 'coffea' with random number
   username: process.env.CHIRPIRC_USERNAME, // default value: username = nick
   realname: process.env.CHIRPIRC_REALNAME, // default value: realname = nick
